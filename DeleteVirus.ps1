4c5be6c2ac994033074bbde820a68aa6d427fc42
Connor Ruggles <cruggles@iastate.edu>
2016-08-01 11:48:13 -0500

    Update DeleteVirus.ps1

--- DeleteVirus.ps1
+++ DeleteVirus.ps1
@@ -4,20 +4,20 @@
 
 
 #path where txt docs are sent
-$LogPath = "\\amessophos\C$\SAV32Logs\Today";
+$LogPath = "\\server\path";
 
 #counting variable
 $script:ScanCount;
 $OldScanCount;
 
 #path where report is sent
-$ReportPath = "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\"
+$ReportPath = "\\server\reportpath"
 
 #path where changed table will go after header modification
-$ToBeScanned = "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReportsOld\"
+$ToBeScanned = "\\server\oldreportpath"
 
 #gets each file in the report folder
-$ToChange = Get-ChildItem -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\"
+$ToChange = Get-ChildItem -Path $ReportPath
 
 #loops through to check for a new report every morning
 ForEach($newFile in $ToChange)
@@ -28,22 +28,22 @@ ForEach($newFile in $ToChange)
     $header = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,ComputerName,22,23,24,25,26"
 
     #gets contents of the report
-    $content = get-content -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\TABLE.csv"
+    $content = get-content -Path $ReportPath\TABLE.csv
 
     #deletes the report because we have what we need
-    Remove-Item -Force -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\TABLE.csv"
+    Remove-Item -Force -Path $ReportPath\TABLE.csv
 
     #sets the header in a new file
-    $header | Set-Content -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\newtable.csv"
+    $header | Set-Content -Path $ReportPath\newtable.csv
 
     #adds the contents of the old report to the new file under the new header
-    Add-Content -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\newtable.csv" -Value $content
+    Add-Content -Path $ReportPath\newtable.csv -Value $content
 
     #gets only the column with the header "ComputerName" since that is all we care about
-    $computers = Import-CSV -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\newtable.csv" | Select ComputerName
+    $computers = Import-CSV -Path $ReportPath\newtable.csv | Select ComputerName
     
     #Moves the new file to a different folder since we are now done with it
-    Move-Item -Path "\\regfilesrvr\Departments\Information Technology\Sophos\SophosReports\newtable.csv" -Destination $ToBeScanned -Force
+    Move-Item -Path $ReportPath\newtable.csv -Destination $ToBeScanned -Force
 
     #loops through each computer name in the list, starting a scan on each
     ForEach($newName in $computers)
